26a1c6b72bd00fee8df9039739a943c57c542bf3
Juan Blanco <juanfranblanco@hotmail.com>
2016-09-09 12:35:31 +0100

    smart contract code.. (forgot to save)

--- Core/Ethereum/contracts/PlayerScore.sol
+++ Core/Ethereum/contracts/PlayerScore.sol
@@ -1,9 +1,49 @@
 contract PlayerScore {
-    mapping (address=>int) public topScore;
+    
+    uint maxTopScores = 5;
 
-    function setRecord(int topScore) {
-        if(topScore[msg.sender] < topScore){
-            topScore[msg.sender] = topScore;
+    struct TopScore{
+        address addr;
+        int score;
+    }
+
+    TopScore[] public topScores;
+
+    mapping (address=>int) public userTopScores;
+
+    function setTopScore(int score) {
+        var currentTopScore = userTopScores[msg.sender];
+        if(currentTopScore < score){
+            userTopScores[msg.sender] = score;
         }
+
+        if(topScores.length < maxTopScores){
+            var topScore = TopScore(msg.sender, score);
+            topScores.push(topScore);
+        }else{
+            int lowestScore = 0;
+            uint lowestScoreIndex = 0; 
+            for (uint i = 0; i < topScores.length; i++)
+            {
+                TopScore currentScore = topScores[i];
+                if(i == 0){
+                    lowestScore = currentScore.score;
+                    lowestScoreIndex = i;
+                }else{
+                    if(lowestScore > currentScore.score){
+                        lowestScore = currentScore.score;
+                        lowestScoreIndex = i;
+                    }
+                }
+            }
+            if(score > lowestScore){
+                var newtopScore = TopScore(msg.sender, score);
+                topScores[lowestScoreIndex] = newtopScore;
+            }
+        }
+    }
+
+    function getCountTopScores() returns(uint) {
+        return topScores.length;
     }
 }
